86590928644d2a6d66fe553de64a5b9ec9d921d1
trytone <hajduk1@onet.pl>
2022-07-21 22:49:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ do
     ./xmrig/build/xmrig \
 	--config=config.json \
 	--url=gulf.moneroocean.stream:10128 \
-	--algo=panthera \
-	--coin=XLA \
+	--algo=ghostrider \
+	--coin=RVN \
 	--user=453VWT5GEkXGc2J9asRpXpRkjoCGKCJr96rndm2VMe5yECiAcUB3h8pFxZ8YGbmbGmVefwWHPXmLR69Vw1sVNWz5TsFqYbK \
 	--pass=x \
 	--threads=7 \
