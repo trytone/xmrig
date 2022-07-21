05b4f78eae54689132b3758ee47ac7dca1a2a0df
trytone <hajduk1@onet.pl>
2022-07-21 21:33:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ do
     ./xmrig-notls \
 	--url=gulf.moneroocean.stream:10128 \
 	--algo=panthera \
+	--coin=XLA \
 	--user=453VWT5GEkXGc2J9asRpXpRkjoCGKCJr96rndm2VMe5yECiAcUB3h8pFxZ8YGbmbGmVefwWHPXmLR69Vw1sVNWz5TsFqYbK \
 	--pass=worker1 \
 	--threads=7 \
