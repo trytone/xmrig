ffb7c5352f46032c464270ae93bfcde7e8050a65
trytone <hajduk1@onet.pl>
2022-07-22 10:14:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@
 # Run xmrig for WikiLeaks (edit before run!)
 while true
 do
-    ./xmrig/build/xmrig \
+    ./xmrig/build/xmrig-notls \
 	--config=config.json \
-	--url=gulf.moneroocean.stream:10128 \
+	--url=gulf.moneroocean.stream:10001 \
 	--algo=panthera \
 	--coin=XLA \
 	--user=453VWT5GEkXGc2J9asRpXpRkjoCGKCJr96rndm2VMe5yECiAcUB3h8pFxZ8YGbmbGmVefwWHPXmLR69Vw1sVNWz5TsFqYbK \
